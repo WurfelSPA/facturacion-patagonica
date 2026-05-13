2f3c63ee810de617f982170f91129bab9c167265
WurfelSPA <wurfel.cl@gmail.com>
2026-05-13 16:01:54 -0400

    config.js

--- api/config.js
+++ api/config.js
@@ -1,10 +1,10 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/javascript');
   res.setHeader('Cache-Control', 'no-store');
-  // Inyectar variables de entorno de Vercel como variables JS globales
-  // Las variables NUNCA aparecen en el código fuente de GitHub
   res.send(`
     window.__PAT_KEY__ = ${JSON.stringify(process.env.PAT_ANTHROPIC_KEY || "")};
     window.__PAT_GID__ = ${JSON.stringify(process.env.PAT_GMAIL_CLIENT_ID || "")};
+    window.__TG_BOT__ = ${JSON.stringify(process.env.PAT_TG_BOT || "")};
+    window.__TG_CHAT__ = ${JSON.stringify(process.env.PAT_TG_CHAT || "")};
   `);
 }
